15f234a2a8ad53891bccb330fd6fff02506721dd
Evgeniy <evgeniy.ryadovoy@rentateam.ru>
2020-01-29 13:57:03 +0600

    carthage lib

---
+++ Carthage/Build/iOS/CommonTools.framework/Headers/CommonTools.h
@@ -0,0 +1,19 @@
+//
+//  CommonTools.h
+//  CommonTools
+//
+//  Created by Evgeniy on 28/01/2020.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonTools.
+FOUNDATION_EXPORT double CommonToolsVersionNumber;
+
+//! Project version string for CommonTools.
+FOUNDATION_EXPORT const unsigned char CommonToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonTools/PublicHeader.h>
+
+
